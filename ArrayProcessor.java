64425728d57e300182b96e660e28719248482693
Henjie Carmelotes <101442508+HenjieCarmelotes@users.noreply.github.com>
2022-12-14 22:38:48 +0700

    An interface needed for CalculatorUsingLambda class

---
+++ ArrayProcessor.java
@@ -0,0 +1,15 @@
+package java2022githubproject;
+
+/**
+ * A functional interface that defines a function
+ * containing one parameter of type double[] and that
+ * returns a value of type double.
+ */
+
+
+public interface ArrayProcessor {
+	
+	double execute( double[] arrayList );
+	
+
+}
