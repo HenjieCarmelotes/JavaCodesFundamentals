49661d97b3109b5b1bf6c6efe689d18a0a315e80
Henjie Carmelotes <101442508+HenjieCarmelotes@users.noreply.github.com>
2023-04-08 22:37:04 +0700

    Implementing a way to print name initials.

---
+++ PrintInitials.java
@@ -0,0 +1,22 @@
+
+public class PrintInitials {
+	/*This program prints my initials (HAC) in print format.
+	 * 
+	 */
+	public static void main(String [] args) {
+		System.out.println();
+		System.out.println("         ***      ***             ******                 ********");
+		System.out.println("         ***      ***          	 ********              ***********");
+		System.out.println("         ***      ***          	****  ****            ***       ***");
+		System.out.println("         ***      ***          ****    ****           ***        **");
+		System.out.println("         ***      ***         ****      ****          ***");
+		System.out.println("         ************        ****************         ***");
+		System.out.println("         ***      ***       *****        *****        ***");
+		System.out.println("         ***      ***      *****          *****       ***        **");
+		System.out.println("         ***      ***     *****            *****      ***       ***");
+		System.out.println("         ***      ***    *****              *****      ***********");
+		System.out.println("         ***      ***   *****                *****       ********");
+		System.out.println();
+	}
+
+}
